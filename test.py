d8891cdd2d1d3218c2e90c8331a4f72c94f48175
oraclespainpresales <oraclespainpresales@gmail.com>
2023-01-16 17:18:59 +0100

    services

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
 fileobj = open('test.zip', 'rb')
-r = requests.post('http://localhost:6000/submit', data={"test": "go"}, files={"archive": ("test.zip", fileobj)})
+r = requests.post('http://localhost:6000/submit', data={"test": "go"}, files={"images": ("test.zip", fileobj)})
 
 print(r.json())
